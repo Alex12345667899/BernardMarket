3ccfcc8751d8fa90f46ddd554c9261f8ce5a059d
enzo bertevas <enzo.bertevas@gmail.com>
2024-08-12 16:55:59 +0200

    init base of the project

---
+++ db/migrate/20240812143935_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.integer :total_price
+      t.boolean :accepted
+      t.references :user, null: false, foreign_key: true
+      t.references :offer, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
